2e288e1ab151e980858550c68dcfa3a806d1e83d
judeadeniji66 <judeadeniji66@gmail.com>
2025-05-20 23:53:57 +0100

    feat: Update client origin configuration to support local and production environments

--- src/server/client.ts
+++ src/server/client.ts
@@ -3,7 +3,9 @@ import type { Server } from "@/server";
 
 console.log("Client URL:", "/api/v1", );
 
-const origin = process.env.NEXT_PUBLIC_URL!;
+const origin = process.env.NEXT_PUBLIC_URL || process.env.NODE_ENV === "production"
+    ? "https://ui-epf.onrender.com"
+    : "http://localhost:3000";
 
 const client = hc<Server>(new URL(origin).toString());
 
